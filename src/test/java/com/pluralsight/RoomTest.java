3f8e443c2b57dfe0a03b667fd5cc84a3ab80d0ce
cealyssa814 <a.hollingsworth814@gmail.com>
2025-10-22 13:47:27 -0400

    Added test file

---
+++ src/test/java/com/pluralsight/RoomTest.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public static void main(String[] args);
+
+@Test
+class RoomTest {
+    public void Should_RoomBeDirtyAndOccupied_True(){
+        //arrange
+        Room 1=new Room;
+        boolean checkInRoom=true;
+        boolean
+
+
+
+    }
+
+}
